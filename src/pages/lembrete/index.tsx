d4e3f64c28c644b7ccf2bf4a25e95acf63fdaa56
Patrick Castro Mendes Lustosa Souza <79330796+LustosaDev@users.noreply.github.com>
2025-05-14 00:22:13 -0300

    add_lembrete
    adição de tela de lembrete e estilização da mesma

--- src/pages/lembrete/index.tsx
+++ src/pages/lembrete/index.tsx
@@ -1,10 +1,58 @@
 import React from "react";
-import {Text} from 'react-native'
+import { Text, View, TextInput, TouchableOpacity} from "react-native";
+import { AntDesign } from '@expo/vector-icons';
+import styles from './style';
 
-export default function Lembrete (){
-    return(
-        <Text>
-            Ola mundo lembrete
-        </Text>
-    )
+export default function LembretesTela() {
+  return (
+    <View style={styles.container}>
+
+      <View style={styles.header}>
+        <TouchableOpacity style={styles.backButton}>
+          <AntDesign name="arrowleft" size={32} color="black" />
+        </TouchableOpacity>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Vacina<Text style={styles.titlePlus}>Plus</Text></Text>
+        </View>
+        
+      </View>
+
+      <Text style={styles.subheader}>Lembretes de Vacinação</Text>
+      
+      <View style={styles.card}>
+
+        <View style={styles.cardHeader}>
+          <Text style={styles.cardTitle}>Seu Lembrete</Text>
+        </View>
+
+        <View style={styles.inputGroup}>
+          <Text style={styles.label}>Vacina</Text>
+          <TextInput style={styles.input} />
+        </View>
+        
+        <View style={styles.inputGroup}>
+          <Text style={styles.label}>Data e Hora</Text>
+          <View style={styles.timeRow}>
+            <TextInput style={styles.smallInput} />
+            <TextInput style={styles.smallInput} />
+            <TextInput style={styles.smallInput} />
+          </View>
+        </View>
+        
+        <View style={styles.inputGroup}>
+          <Text style={styles.label}>Unidade Médica</Text>
+          <TextInput style={styles.input} />
+        </View>
+        
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Salvar</Text>
+        </TouchableOpacity>
+      </View>
+      
+
+    </View>
+  );
 }
--- src/pages/lembrete/style.ts
+++ src/pages/lembrete/style.ts
@@ -1,6 +1,128 @@
-import { StyleSheet,Dimensions } from "react-native";
-import { themas } from "../../global/themes";
+import { StyleSheet } from "react-native";
 
-export const style = StyleSheet.create({
-    
-})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 16,
+    justifyContent: 'space-between',
+  },
+
+  header: {
+    backgroundColor: 'white',
+    paddingTop: 50, 
+    paddingBottom: 16,
+    paddingHorizontal: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between', 
+  },
+
+  backButton: {
+    marginRight: 8,
+  },
+
+  titleContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+
+  title: {
+    fontSize: 32,
+    fontWeight: 'semibold',
+  },
+
+  titlePlus: {
+    color: '#004AD2',
+  },
+
+  subheader: {
+    color: '#003366',
+    fontSize: 24,
+    textAlign: 'center',
+    marginVertical: 10,
+  },
+
+  card: {
+    backgroundColor: '#6A9CF8',
+    borderRadius: 20,
+    overflow: 'hidden',
+    padding: 16,
+    marginVertical: 20,
+    elevation: 4,
+  },
+
+  cardHeader: {
+    backgroundColor: '#236EFA',
+    paddingVertical: 10,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    alignItems: 'center',
+    marginHorizontal: -16, 
+    marginTop: -16,
+    marginBottom: 10,
+  },
+
+  cardTitle: {
+    color: '#F5F5F5',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+
+  label: {
+    fontSize: 26,
+    fontWeight: '500',
+    marginBottom: 5,
+    marginTop: 10,
+  },
+
+  inputGroup: {
+    marginBottom: 16,
+  },
+
+  input: {
+    backgroundColor: '#FFFFFF',
+    padding: 12,
+    paddingVertical: 8,
+    borderRadius: 6,
+    fontSize: 16,
+  },
+
+  timeRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    gap: 5,
+    marginBottom: 12,
+  },
+
+  smallInput: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 6,
+    paddingVertical: 8,
+    paddingHorizontal: 8,
+  },
+
+  buttonContainer: {
+    alignItems: 'center',
+  },
+
+  button: {
+    backgroundColor: '#236EFA',
+    paddingHorizontal: 50,
+    paddingVertical: 14,
+    borderRadius: 4,
+    alignItems: 'center',
+    marginTop: 8,
+    width: '50%',
+    elevation: 5,
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+
+});
+
+export default styles;
